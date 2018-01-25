fc81c1addffb55b1fb55938ae93364fd9985695f
Jacob Zhang <274381933@qq.com>
2018-01-25 14:15:55 +0800

    Initial commit

---
+++ src/java8/stream/allMatch.java
@@ -0,0 +1,16 @@
+package java8.stream;
+
+/**
+ * @ProjectName: java_basics
+ * @Package: java8.stream
+ * @Description:
+ * @Author: Jacob Zhang
+ * @CreateDate: 2018/1/25/025 14:14
+ * @UpdateDate: 2018/1/25/025 14:14
+ */
+public class allMatch {
+
+    public static void main(String[] args) {
+
+    }
+}
